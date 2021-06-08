fc21aa0ee3a5ee4496b42df80c07e889c4ee8088
김민수 <kokbee@kokbee.local>
2021-06-08 13:56:15 +0900

    modify code & env

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 # code : kokbee
 
-import sys,os,re
-from flask import Flask
+from flask import Flask, jsonify, request
+import datetime
 
 app = Flask(__name__)
  
@@ -10,3 +10,32 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return 'Clone Flask'
+
+@app.route("/api/years", methods=['GET', 'POST'])
+def years():
+    '''
+    년도의 범위 API
+    '''
+    nowdatetime = datetime.datetime.now()
+    nowdate = nowdatetime.strftime("%Y-%m-%d %H:%M:%S")
+
+    years = []
+    # get -3 ~ +3
+    if request.method == "GET":
+        for y in range(nowdatetime.year-3, nowdatetime.year+3):
+            if not y in years:
+                years.append(str(y))
+    # post
+    elif request.method == "POST":
+        postData = request.get_data()
+        print (postData)
+        start = postData['Start']
+        end = postData['End']
+        for y in range(start, end):
+            if not y in years:
+                years.append(str(y))
+
+    data = {}
+    data.update({"data" : years})
+
+    return jsonify(data)
--- run.sh
+++ run.sh
@@ -1,6 +1,1 @@
-FLASK_APP="app"
-FLASK_DEBUG="True"
-
-export env=$env:$FLASK_APP:$FLASK_DEBUG
-
-flask run --host=0.0.0.0
+source ./env && flask run --host=0.0.0.0
